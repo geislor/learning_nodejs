94b468bcad733839ef4f627fd4b18aa157ce945a
Geislor Crestani <geislor@gmail.com>
2018-07-28 09:59:57 -0300

    http server reading url from request

---
+++ http-server-1.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var server = http.createServer( function(req, res){
+    var url = req.url;
+    
+    if (url == '/test'){
+        res.end("<html><body>URL TEST 1</body</html>");
+
+    }else{
+        res.end("<html><body>Hello world</body></html>");
+    }
+})
+
+server.listen(3000); 
